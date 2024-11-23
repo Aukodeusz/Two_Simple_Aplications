270e1034a52774a404d42e4e3b28c371344b6655
VICTOR <gurongai514@onet.pl>
2024-11-23 14:22:06 +0100

    Dodanie głównej aktywności aplikacji łączącej cały program

--- app/src/main/java/com/example/movietraker/MainActivity.kt
+++ app/src/main/java/com/example/movietraker/MainActivity.kt
@@ -1,20 +1,65 @@
-package com.example.movietraker
+
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var itemAdapter: ItemAdapter
+    private var itemList: MutableList<Item> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        recyclerView = findViewById(R.id.recyclerView)
+        itemAdapter = ItemAdapter(itemList)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = itemAdapter
+
+        val titleEditText: EditText = findViewById(R.id.editText_title)
+        val genreEditText: EditText = findViewById(R.id.editText_genre)
+        val descriptionEditText: EditText = findViewById(R.id.editText_description)
+        val ratingSeekBar: SeekBar = findViewById(R.id.seekBar_rating)
+        val addButton: Button = findViewById(R.id.button_add)
+        val radioGroup: RadioGroup = findViewById(R.id.radioGroup)
+
+        loadData()
+
+        addButton.setOnClickListener {
+            val title = titleEditText.text.toString()
+            val genre = genreEditText.text.toString()
+            val description = descriptionEditText.text.toString()
+            val rating = ratingSeekBar.progress
+            val isMovie = radioGroup.checkedRadioButtonId == R.id.radio_movie
+
+            val newItem = Item(title, genre, description, rating, isMovie)
+            itemList.add(newItem)
+            itemAdapter.notifyDataSetChanged()
+            saveData()
         }
+    }
+
+    private fun saveData() {
+        val sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        val gson = Gson()
+        val json = gson.toJson(itemList)
+        editor.putString("item list", json)
+        editor.apply()
+    }
+
+    private fun loadData() {
+        val sharedPreferences = getSharedPreferences("shared preferences", MODE_PRIVATE)
+        val gson = Gson()
+        val json = sharedPreferences.getString("item list", null)
+        val type = object : TypeToken<ArrayList<Item>>() {}.type
+        itemList = gson.fromJson(json, type) ?: mutableListOf()
     }
 }
